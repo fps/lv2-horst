46c54d8db686e4fe84656de776a63e30c56a94b7
Florian Paul Schmidt <mista.tapas@gmx.net>
2023-04-12 10:57:44 +0200

    use client name if given

--- src/horst_cli.cc
+++ src/horst_cli.cc
@@ -7,7 +7,9 @@
 int main (int argc, char *argv[]) {
   horst::horst h;
   if (argc < 2) throw std::runtime_error ("Missing argument: LV2 plugin URI");
-  horst::unit_ptr unit = h.lv2(argv[1], "test", false);
+  std::string client_name = "horst_cli";
+  if (argc > 2) client_name = argv[2];
+  horst::unit_ptr unit = h.lv2(argv[1], client_name,  false);
 
   std::string input;
   std::cin >> input;
