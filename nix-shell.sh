f26c5752ba0dcd26b9ebe5f73daa261228ea0c41
Florian Paul Schmidt <mista.tapas@gmx.net>
2023-04-13 09:47:32 +0200

    clean up nix-shell.sh

--- nix-shell.sh
+++ nix-shell.sh
@@ -1,1 +1,1 @@
-nix-shell -p gnumake ladspa-sdk stdenv readline jack2 jalv ladspa-sdk python310 python310Packages.pybind11 pkg-config lilv valgrind gdb linuxKernel.packages.linux_5_15.perf python310Packages.setuptools python310Packages.pkgconfig
+nix-shell -p gnumake stdenv jack2 python310 python310Packages.pybind11 pkg-config lilv valgrind gdb linuxKernel.packages.linux_5_15.perf 
