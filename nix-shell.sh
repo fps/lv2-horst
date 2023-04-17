4657539771e144fb55f57993c7bd72dda09d66f7
Florian Paul Schmidt <mista.tapas@gmx.net>
2023-04-17 22:36:03 +0200

    add ipywrap

--- nix-shell.sh
+++ nix-shell.sh
@@ -1,1 +1,1 @@
-nix-shell -p gnumake stdenv jack2 python310 python310Packages.pybind11 pkg-config lilv valgrind gdb linuxKernel.packages.linux_5_15.perf 
+nix-shell -p gnumake stdenv jack2 python310 python310Packages.pybind11 python310Packages.ipython pkg-config lilv valgrind gdb linuxKernel.packages.linux_5_15.perf 
