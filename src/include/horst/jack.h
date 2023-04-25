0cbfd454c71ef230eaa6b3b16e606cb93da6bbca
Florian Paul Schmidt <mista.tapas@gmx.net>
2023-04-25 10:44:34 +0200

    return to the one and only true style

--- src/include/horst/jack.h
+++ src/include/horst/jack.h
@@ -2,13 +2,15 @@
 
 namespace horst
 {
-  extern "C" {
+  extern "C" 
+  {
     int unit_jack_process_callback (jack_nframes_t nframes, void *arg);
     int unit_jack_buffer_size_callback (jack_nframes_t buffer_size, void *arg);
     int unit_jack_sample_rate_callback (jack_nframes_t sample_rate, void *arg);
   }
 
-  struct raw_jack_client {
+  struct raw_jack_client 
+  {
     jack_client_t *m_jack_client;
 
     jack_nframes_t m_sample_rate;
@@ -26,24 +28,19 @@ namespace horst
       int ret;
 
       ret = jack_set_sample_rate_callback (m_jack_client, unit_jack_sample_rate_callback, (void *)this);
-      if (ret != 0) {
-        throw std::runtime_error ("horst: plugin_unit: Failed to set sample rate callback");
-      }
+      if (ret != 0) throw std::runtime_error ("horst: plugin_unit: Failed to set sample rate callback");
 
       ret = jack_set_process_callback (m_jack_client, unit_jack_process_callback, (void *)this);
-      if (ret != 0) {
-        throw std::runtime_error ("horst: plugin_unit: Failed to set buffer size callback");
-      }
+      if (ret != 0) throw std::runtime_error ("horst: plugin_unit: Failed to set buffer size callback");
 
       ret = jack_set_buffer_size_callback (m_jack_client, unit_jack_buffer_size_callback, (void *)this);
-      if (ret != 0) {
-        throw std::runtime_error ("horst: plugin_unit: Failed to set buffer size callback");
-      }
+      if (ret != 0) throw std::runtime_error ("horst: plugin_unit: Failed to set buffer size callback");
 
       DBG_EXIT
     }
 
-    virtual std::string get_jack_client_name () {
+    virtual std::string get_jack_client_name () 
+    {
       return jack_get_client_name (m_jack_client);
     }
 
@@ -90,16 +87,19 @@ namespace horst
 
   typedef std::shared_ptr<raw_jack_client> raw_jack_client_ptr;
 
-  struct jack_client : public raw_jack_client {
+  struct jack_client : public raw_jack_client 
+  {
     jack_client (const std::string &client_name, jack_options_t options) :
-      raw_jack_client (jack_client_open (client_name.c_str (), options, 0)) {
+      raw_jack_client (jack_client_open (client_name.c_str (), options, 0)) 
+    {
       DBG("name: \"" << client_name.c_str() << "\" options: " << options)
 
       if (m_jack_client == 0) throw std::runtime_error ("horst: jack_client: Failed to open. Name: " + client_name);
       DBG_EXIT
     }
 
-    ~jack_client () {
+    ~jack_client () 
+    {
       DBG_ENTER
       jack_client_close (m_jack_client);
       DBG_EXIT
--- src/include/horst/lv2.h
+++ src/include/horst/lv2.h
@@ -1,11 +1,13 @@
 #pragma once
 
-namespace horst {
-
-  struct lilv_world {
+namespace horst 
+{
+  struct lilv_world 
+  {
     LilvWorld *m;
 
-    lilv_world () {
+    lilv_world () 
+    {
       DBG_ENTER
       m = lilv_world_new ();
       if (m == 0) throw std::runtime_error ("horst: lilv_world: Failed to create lilv world");
@@ -14,7 +16,8 @@ namespace horst {
       DBG_EXIT
     }
 
-    ~lilv_world () {
+    ~lilv_world () 
+    {
       DBG_ENTER
       DBG((void*)m)
       lilv_world_free (m);
@@ -24,24 +27,28 @@ namespace horst {
 
   typedef std::shared_ptr<lilv_world> lilv_world_ptr;
 
-  struct lilv_plugins {
+  struct lilv_plugins 
+  {
     const LilvPlugins *m;
     lilv_world_ptr m_world;
 
     lilv_plugins (lilv_world_ptr world) :
       m (lilv_world_get_all_plugins (world->m)),
-      m_world (world) {
+      m_world (world) 
+    {
       DBG_ENTER_EXIT
     }
 
-    ~lilv_plugins () {
+    ~lilv_plugins () 
+    {
       DBG_ENTER_EXIT
     }
   };
 
   typedef std::shared_ptr<lilv_plugins> lilv_plugins_ptr;
 
-  struct lilv_uri_node {
+  struct lilv_uri_node 
+  {
     const std::string m_uri;
     lilv_world_ptr m_world;
     LilvNode *m;
@@ -49,18 +56,21 @@ namespace horst {
     lilv_uri_node (lilv_world_ptr world, const std::string &uri) :
       m_uri (uri),
       m_world (world),
-      m (lilv_new_uri (world->m, uri.c_str ())) {
+      m (lilv_new_uri (world->m, uri.c_str ())) 
+    {
       if (m == 0) throw std::runtime_error ("horst: lilv_uri_node: Failed to create lilv uri node. URI: " + uri);
     }
 
-    ~lilv_uri_node () {
+    ~lilv_uri_node () 
+    {
       lilv_node_free (m);
     }
   };
 
   typedef std::shared_ptr<lilv_uri_node> lilv_uri_node_ptr;
 
-  struct lilv_plugin {
+  struct lilv_plugin 
+  {
     const LilvPlugin *m;
     lilv_uri_node_ptr m_uri_node;
     lilv_plugins_ptr m_lilv_plugins;
@@ -68,7 +78,8 @@ namespace horst {
     lilv_plugin (lilv_plugins_ptr plugins, lilv_uri_node_ptr node) :
       m (lilv_plugins_get_by_uri (plugins->m, node->m)),
       m_uri_node (node),
-      m_lilv_plugins (plugins) {
+      m_lilv_plugins (plugins) 
+    {
       DBG_ENTER
       if (m == 0) throw std::runtime_error ("horst: lilv_plugin: Plugin not found. URI: " + m_uri_node->m_uri);
       DBG_EXIT
@@ -77,7 +88,8 @@ namespace horst {
 
   typedef std::shared_ptr<lilv_plugin> lilv_plugin_ptr;
 
-  struct lilv_plugin_instance {
+  struct lilv_plugin_instance 
+  {
     LilvInstance *m;
     LV2_Handle m_lv2_handle;
     lilv_plugin_ptr m_plugin;
@@ -95,7 +107,8 @@ namespace horst {
       m_lv2_handle = lilv_instance_get_handle (m);
       DBG(m)
 
-      for (size_t port_index = 0; port_index < m_initial_port_buffers.size (); ++port_index) {
+      for (size_t port_index = 0; port_index < m_initial_port_buffers.size (); ++port_index) 
+      {
         lilv_instance_connect_port (m, port_index, &m_initial_port_buffers[port_index][0]);
       }
 
@@ -103,7 +116,8 @@ namespace horst {
       DBG_EXIT
     }
 
-    ~lilv_plugin_instance () {
+    ~lilv_plugin_instance () 
+    {
       DBG_ENTER
       lilv_instance_deactivate (m);
       lilv_instance_free (m);
